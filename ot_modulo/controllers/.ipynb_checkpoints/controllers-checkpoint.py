6f967641800d9831cce3671a0e5b0792517f6f49
hogan26 <nelson.gonzalezguerra@gmail.com>
2020-06-30 02:23:48 +0000

    modulo de ordenes de trabajo

---
+++ ot_modulo/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+ from odoo import http
+
+
+ class OtModulo(http.Controller):
+     @http.route('/ot_modulo/ot_modulo/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
+
+     @http.route('/ot_modulo/ot_modulo/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('ot_modulo.listing', {
+             'root': '/ot_modulo/ot_modulo',
+             'objects': http.request.env['ot_modulo.ot_modulo'].search([]),
+         })
+
+     @http.route('/ot_modulo/ot_modulo/objects/<model("ot_modulo.ot_modulo"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('ot_modulo.object', {
+             'object': obj
+         })
