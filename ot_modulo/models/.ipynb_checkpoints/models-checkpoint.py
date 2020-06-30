d945dab858ee227c5fd5a9c2e16926d0082ca5b0
hogan26 <nelson.gonzalezguerra@gmail.com>
2020-06-30 18:19:06 +0000

    cargando prueba

--- ot_modulo/models/.ipynb_checkpoints/models-checkpoint.py
+++ ot_modulo/models/.ipynb_checkpoints/models-checkpoint.py
@@ -8,12 +8,16 @@ class ot_modulo(models.Model):
     _name = 'ot_modulo.ot_modulo'
     _description = 'ot_modulo.ot_modulo'
 
+    id_ot = fields.Integer()
     name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    cliente = fields.Char()
+    estado = fields.Char()
+    equipo = fields.Char()
+    #value = fields.Integer()
+    #value2 = fields.Float(compute="_value_pc", store=True)
+    descripcion = fields.Text()
     #agregando un nuevo campo... campo de fecha y hora
-    start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+    fecha = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
     
     @api.model
     def create(self, values):
